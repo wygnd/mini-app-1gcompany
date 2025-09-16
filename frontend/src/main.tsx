d9fe7ce41de01aebf579ddcb477979e879cd0a45
benchik <wygndo@gmail.com>
2025-09-16 20:17:58 +0300

    remove mockEnv and debug on frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,34 +1,21 @@
 import ReactDOM from 'react-dom/client'
-import './index.css'
 import {EnvUnsupported} from "./components/EnvUnsupported.tsx";
-import {init} from "./init.ts";
-import {retrieveLaunchParams} from "@telegram-apps/sdk";
+import {miniApp} from "@telegram-apps/sdk";
 import {StrictMode} from "react";
-import App from "./components/App.tsx";
 
 // Mock the environment in case, we are outside Telegram.
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 
-try {
-	const launchParams = retrieveLaunchParams();
-	const {tgWebAppPlatform: platform} = launchParams;
-	const debug = (launchParams.tgWebAppStartParam || '').includes('platformer_debug')
-		|| import.meta.env.DEV;
+if (miniApp.mountSync.isAvailable()) {
+	miniApp.mountSync();
+	miniApp.isMounted(); // true
 
-	// Configure all application dependencies.
-	await init({
-		debug,
-		eruda: debug && ['ios', 'android'].includes(platform),
-		mockForMacOS: platform === 'macos',
-	})
-		.then(() => {
-			root.render(
-				<StrictMode>
-					<App/>
-				</StrictMode>,
-			);
-		});
-} catch (error) {
+	root.render(
+		<StrictMode>
+			Hello world
+		</StrictMode>,
+	);
+} else {
 	root.render(<EnvUnsupported/>);
 }
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-	build: {
-		minify: "terser",
-	},
 	optimizeDeps: {
 		exclude: ["@telegram-apps/sdk", "@telegram-apps/sdk-react"],
 	},
