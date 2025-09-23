dc2a2875c60fc9c09b308eada8658beb8368d18a
benchik <wygndo@gmail.com>
2025-09-23 20:59:18 +0300

    fix login

--- frontend/src/http/usersApi.ts
+++ frontend/src/http/usersApi.ts
@@ -2,6 +2,6 @@ import {$API} from "@/http/index.ts";
 import {UserInterface} from "@/types/user.ts";
 
 export async function login() {
-	const {data} = await $API.get<UserInterface>("/users/login");
+	const {data} = await $API.post<UserInterface>("/users/login");
 	return data;
 }
