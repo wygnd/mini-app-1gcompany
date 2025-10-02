609b61d1350dd3f9e87bee2e923424d5748ab708
benchik <wygndo@gmail.com>
2025-10-02 21:29:25 +0300

    fix redis unkonwn dependencies exception

--- backend/src/modules/refunds/refunds.module.ts
+++ backend/src/modules/refunds/refunds.module.ts
@@ -3,9 +3,10 @@ import {DatabaseModule} from "../database/database.module";
 import {RefundsService} from "./refunds.service";
 import {refundsProviders} from "./refunds.providers";
 import {RefundsController} from "./refunds.controller";
+import {RedisModule} from "../redis/redis.module";
 
 @Module({
-	imports: [DatabaseModule],
+	imports: [DatabaseModule, RedisModule],
 	controllers: [RefundsController],
 	providers: [...refundsProviders, RefundsService],
 })
