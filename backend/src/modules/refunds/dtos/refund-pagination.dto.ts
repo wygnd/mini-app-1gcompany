6af1fb6c3fc9254e22c63ca51f6b4959c74ee660
benchik <wygndo@gmail.com>
2025-10-10 07:17:49 +0300

    add erros on sort key

--- backend/src/modules/refunds/dtos/refund-pagination.dto.ts
+++ backend/src/modules/refunds/dtos/refund-pagination.dto.ts
@@ -1,7 +1,10 @@
 import {PaginationDto} from "../../../common/dto/pagination.dto";
 import {IsSortKey} from "../../../common/validators/is-sort-key.validator";
+import {IsOptional, IsString} from "class-validator";
 
 export class RefundPaginationDto extends PaginationDto {
 	@IsSortKey(['refundId', 'createdAt', 'updatedAt', 'status'])
+	@IsOptional({message: "Invalid key of sorting"})
+	@IsString({message: "Sort key must be a string"})
 	declare sort: string;
 }
